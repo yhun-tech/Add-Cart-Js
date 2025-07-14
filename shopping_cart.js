c989df90006b4319cf5904c2cbc67111794b372d
yhun <jhunguidef@gmail.com>
2025-07-15 00:50:19 +0800

    3rd Commit Add Javascript Remove and Calculate Items total

--- shopping_cart.js
+++ shopping_cart.js
@@ -1,28 +1,88 @@
-addEventListener("DOMContentLoaded" , ()=>{
+if(document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded",ready)
+}else{
+
+    ready()
+
+}
 
 
-    const addtoCartbtn = document.querySelector(".cart")
-    const productBtn =document.querySelector(".product_frame")
+function ready(){   
 
-
-    addtoCartbtn.addEventListener("click", (event)=>{
-         event.preventDefault()
-
-            console.log("Add to Cart Btn Was Clicked")
-    })
-
-
-    productBtn.addEventListener("click", (event)=>{
-
-         event.preventDefault()
-
-          console.log("productBtn Was Clicked")
-
-
-    })
-
+    const removeCartItemBtn =  document.getElementsByClassName("remove")
     
+    // REMOVE ITEM BUTTON
+    for(let i =0; i < removeCartItemBtn.length; i++){
+            let button = removeCartItemBtn[i]
+            button.addEventListener("click", removeCartItem)
+    }
+
+    let quantityInputs = document.getElementsByClassName("quantity")
+    for(let i = 0; i < quantityInputs.length; i++){
+        let input = quantityInputs[i]
+        console.log(input)
+
+        input.addEventListener("change",quantityChanged)
+    }
+
+
+}
+
+function removeCartItem(event){
+
+        event.preventDefault()
+        let targetButton = event.target
+        targetButton.parentElement.parentElement.remove()
+        
+        updateCartTotal()
+
+}
+
+function quantityChanged(event){
+    let input = event.target
+
+    if(isNaN(input.value) || input.value <= 0){
+        input.value = 1
+    }
+    updateCartTotal()
+
+}
 
 
 
-})
+
+
+
+
+
+
+
+function updateCartTotal(){
+    
+    const cartRows = document.getElementsByClassName("data_tr")
+    const totalPrice = document.getElementById("total_price")
+
+    let total = 0;
+    for (let i =0;i < cartRows.length;i++){
+            let cartRow = cartRows[i]
+            
+            let priceElement = cartRow.getElementsByClassName("td_price")[0]
+            let quantityElement = cartRow.getElementsByClassName("quantity")[0]
+
+
+            let price = parseFloat(priceElement.innerText.replace("$",""))
+            let quantity = parseInt(quantityElement.value)
+
+            total += (price * quantity)
+        
+            // console.log(price,quantity)
+
+    }
+    totalPrice.textContent = `$${total}`
+
+
+}
+updateCartTotal()
+
+
+
