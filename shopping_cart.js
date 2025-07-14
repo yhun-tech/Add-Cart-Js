352402ab3c7a6030fd3f14cce7b88002bce0704c
yhun <jhunguidef@gmail.com>
2025-07-14 16:31:46 +0800

    2nd Commit Add Code base item to the cart

--- shopping_cart.js
+++ shopping_cart.js
@@ -1,11 +1,27 @@
 addEventListener("DOMContentLoaded" , ()=>{
 
 
+    const addtoCartbtn = document.querySelector(".cart")
+    const productBtn =document.querySelector(".product_frame")
+
+
+    addtoCartbtn.addEventListener("click", (event)=>{
+         event.preventDefault()
+
+            console.log("Add to Cart Btn Was Clicked")
+    })
+
+
+    productBtn.addEventListener("click", (event)=>{
+
+         event.preventDefault()
+
+          console.log("productBtn Was Clicked")
+
+
+    })
 
     
-
-
-
 
 
 
